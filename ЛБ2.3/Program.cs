c002f669032ab8cde1e4a8c7c58228f1a7375330
naste <naste@DESKTOP-MP6RP0S>
2024-11-10 22:10:08 +0200

    Добавьте файлы проекта.

---
+++ ЛБ2.3/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Введите стороны треугольника");
+Console.Write("Введите сторону a: ");
+double a = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите сторону b: ");
+double b = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите сторону c: ");
+double c = Convert.ToDouble(Console.ReadLine());
+if (a == b || a == c || b == c)
+{
+    Console.WriteLine("Треугольник является равнобедренным.");
+}
+else
+{
+    Console.WriteLine("Треугольник не является равнобедренным.");
+}
---
+++ ЛБ3.1/Program.cs
@@ -0,0 +1,36 @@
+﻿Console.WriteLine("Выберите набор чисел (1, 2 или 3):");
+int x = int.Parse(Console.ReadLine());
+double y;
+switch (x)
+{
+    case 1:
+        {
+            double a = 3;
+            double b = 3.5;
+            double c = -2.1;
+            y = (a + b + c) / 3;
+            Console.WriteLine("Среднее значение 1: " + y);
+            break;
+        }
+    case 2:
+        {
+            double a = 2.1;
+            double b = -6.55;
+            double c = 0.1;
+            y = (a + b + c) / 3;
+            Console.WriteLine("Среднее значение 2: " + y);
+            break;
+        }
+    case 3:
+        {
+            double a = -9;
+            double b = -3.7;
+            double c = -0.1;
+            y = (a + b + c) / 3;
+            Console.WriteLine("Среднее значение 3: " + y);
+            break;
+        }
+    default:
+        Console.WriteLine("Неверный выбор. Выберите 1, 2 или 3.");
+        break;
+}
