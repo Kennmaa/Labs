bf6923ad9cf4990e90b27b006cd6e16768f232fe
Анастасия Самойлова <nastenapy1618@gmail.com>
2024-11-14 20:49:38 +0200

    .

---
+++ Lab3.2/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.WriteLine("Введите значение n:");
+int n;
+if (!int.TryParse(Console.ReadLine(), out n) || n <= 0)
+{
+    Console.WriteLine("Ошибка: введите положительное целое число.");
+    return;
+}
+
+double Q = 0; //сумма 
+
+for (int k = 1; k <= n; k++)
+{
+    double y = Math.Pow(-1, k) * (k - 7); // Числитель
+    double d = 2 * (n - k);             // Знаменатель
+    if (d == 0 || double.IsInfinity(n / d)) // Пропуск слагаемых
+        continue;
+    Q += n / d;
+}
+Console.WriteLine("Сумма ряда Q: " + Q);
