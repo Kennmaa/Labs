57a71803e76602d3dcc6fd925420c2bb162c17ff
Анастасия Самойлова <nastenapy1618@gmail.com>
2024-12-20 15:09:48 +0200

    ю

---
+++ Lab5.1/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Введите символы первого массива:");
+string array1 = Console.ReadLine();
+
+Console.WriteLine("Введите символы второго массива:");
+string array2 = Console.ReadLine();
+
+Console.WriteLine("Общие символы:");
+foreach (char c in array1)
+{
+    if (array2.Contains(c))
+    {
+        Console.WriteLine(c);
+    }
+}
