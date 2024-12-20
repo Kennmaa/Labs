26dc74e0b4286a776f0dbded654da39e2914c569
Анастасия Самойлова <nastenapy1618@gmail.com>
2024-12-20 15:19:40 +0200

    .

---
+++ Lab5.2/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Введите строку:");
+string i = Console.ReadLine();
+
+int A = 0;
+
+foreach (char c in i)
+{
+    if (c == 'А' || c == 'а')
+    {
+        A++;
+    }
+}
+Console.WriteLine($"Количество русских букв 'А' в строке: {A}");
