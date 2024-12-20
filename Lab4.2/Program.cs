18daaa8d58bc585135f13f1a1432f1ebff6ffaef
Анастасия Самойлова <nastenapy1618@gmail.com>
2024-12-20 15:01:26 +0200

    .

---
+++ Lab4.2/Program.cs
@@ -0,0 +1,12 @@
+﻿int n = 3;
+int m = 4;
+int[,] A = new int[n, m];
+for (int i = 0; i < n * m; i++)
+{
+    int s = i / m;
+    int c = i % m;
+    A[s, c] = s * c;
+    Console.Write(A[s, c] + "\t");
+    if (c == m - 1)
+        Console.WriteLine();
+}
