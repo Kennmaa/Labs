1dcc32f0b33b9501fb6dccd1f9006cdfe3f67ef5
Kenma <nastenapy1618@gmail.com>
2025-03-28 06:03:36 +0200

    Add files via upload

---
+++ Lab9.1/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+public class RightTriangle
+{
+    public double FirstLeg { get; private set; }
+    public double SecondLeg { get; private set; }
+
+    public RightTriangle(double firstLeg, double secondLeg)
+    {
+        if (firstLeg <= 0 || secondLeg <= 0)
+        {
+            throw new ArgumentException("Катеты должны быть положительными числами.");
+        }
+        FirstLeg = firstLeg;
+        SecondLeg = secondLeg;
+    }
+    public string GetInfo()
+    {
+        return $"Прямоугольный треугольник с катетами: {FirstLeg} и {SecondLeg}";
+    }
+    public double CalculateTangent()
+    {
+        return SecondLeg / FirstLeg;
+    }
+}
+public class Program
+{
+    public static void Main()
+    {
+        try
+        {
+            RightTriangle triangle = new RightTriangle(3, 4);
+            Console.WriteLine(triangle.GetInfo());
+            double tangent = triangle.CalculateTangent();
+            Console.WriteLine($"Тангенс угла, противолежащего второму катету: {tangent}");
+        }
+        catch (ArgumentException ex)
+        {
+            Console.WriteLine($"Ошибка: {ex.Message}");
+        }
+    }
+}
