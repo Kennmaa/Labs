f4c1ac42b008a6b37cc33152545b2a279b81ad8f
Kenma <nastenapy1618@gmail.com>
2025-03-28 05:11:55 +0200

    Add files via upload

---
+++ Lab8.1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+class SimpleAlarmClock
+{
+    static void Main()
+    {
+        // 1. Разбор времени на компоненты
+        Console.WriteLine("Введите время (формат ЧЧ:ММ:СС.ммм):");
+        if (!TimeSpan.TryParse(Console.ReadLine(), out TimeSpan time))
+        {
+            Console.WriteLine("Ошибка формата времени!");
+            return;
+        }
+
+        Console.WriteLine($"\nРазбор времени:\nЧасы: {time.Hours}\nМинуты: {time.Minutes}\nСекунды: {time.Seconds}\nМиллисекунды: {time.Milliseconds}");
+
+        // 2. Будильник
+        Console.WriteLine("\nВведите время будильника (формат ЧЧ:ММ):");
+        if (!TimeSpan.TryParse(Console.ReadLine(), out TimeSpan alarmTime))
+        {
+            Console.WriteLine("Ошибка формата будильника!");
+            return;
+        }
+
+        DateTime now = DateTime.Now;
+        DateTime alarm = now.Date.Add(alarmTime);
+
+        if (alarm < now) alarm = alarm.AddDays(1);
+
+        Console.WriteLine($"\nДо будильника: {(int)(alarm - now).TotalMinutes} мин.");
+        Console.WriteLine($"Сработает на {alarmTime.Hours * 60 + alarmTime.Minutes} минуте суток.");
+    }
+}
